c23bab027e22d829fafa69a99e07ef2c89512020
merlox <merunasgrincalaitis@gmail.com>
2019-09-01 16:45:37 +0200

    Added yargs and server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { join } = require('path')
+const app = express()
+const yargs = require('yargs')
+const argv = yargs.option('port', {
+    alias: 'p',
+    description: 'Set the port to run this server on',
+    type: 'number',
+}).help().alias('help', 'h').argv
+if(!argv.port) {
+    console.log('Error, you need to pass the port you want to run this application on with npm start -- -p 8001')
+    process.exit(0)
+}
+const port = argv.port
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use('*', (req, res, next) => {
+	// Logger
+	let time = new Date()
+	console.log(`${req.method} to ${req.originalUrl} at ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`)
+	next()
+})
+app.use(express.static('dist'))
+app.listen(port, '0.0.0.0', (req, res) => {
+	console.log(`Listening on localhost:${port}`)
+})
